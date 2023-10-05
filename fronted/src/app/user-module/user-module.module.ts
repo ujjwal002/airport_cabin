3f1421fcc71dd21ab5cb0b9424053bd801132435
Ujjwal Kumar <ujjwalsinghrajput2000@gmail.com>
2023-10-05 16:47:11 +0530

    Added a userModule and adminModule

---
+++ fronted/src/app/user-module/user-module.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserModuleRoutingModule } from './user-module-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    UserModuleRoutingModule
+  ]
+})
+export class UserModuleModule { }
